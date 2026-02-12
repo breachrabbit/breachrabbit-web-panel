24edeb0f7818a9fe5150817d0bb00c8c40998fbc
Breach Rabbit <voznyuk@breachrabbit.ru>
2026-02-12 14:05:59 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -41,7 +41,7 @@ print_info "Step 2/10: Adding repositories..."
 add-apt-repository ppa:ondrej/php -y
 
 # MariaDB 11.4
-curl -LsS https://r.mariadb.com/downloads/mariadb_repo_setup | bash -s -- --mariadb-server-version="mariadb-11.4.10"
+curl -LsS https://r.mariadb.com/downloads/mariadb_repo_setup | bash -s -- --mariadb-server-version="mariadb:latest"
 
 # OpenLiteSpeed 1.8
 wget -O - https://rpms.litespeedtech.com/debian/enable_lst_debian_repo.sh | bash
