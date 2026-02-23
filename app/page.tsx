36929f07712d17c8ea3aa0376522ad48a170cedc
Breach Rabbit <voznyuk@breachrabbit.ru>
2026-02-23 15:55:58 +0300

    Update

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,5 @@
-export default function Home() {
-  return (
-    <div style={{ padding: 40 }}>
-      <h1>BreachPanel MVP is running 🚀</h1>
-      <p>This is a test installation build.</p>
-    </div>
-  );
+import { redirect } from 'next/navigation';
+
+export default function RootPage() {
+  redirect('/dashboard');
 }
